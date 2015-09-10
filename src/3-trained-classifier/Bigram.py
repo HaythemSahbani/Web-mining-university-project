58626e757b463f2aec6751e04fbaf0e83cf0adf9
Haythem Sahbani <sahbani.haythem@gmail.com>
2015-09-10 12:12:59 +0200

    Create Bigram.py

---
+++ src/3-trained-classifier/Bigram.py
@@ -0,0 +1,55 @@
+__author__ = 'Atef Bellaaj'
+__author__ = 'Bellaaj'
+import collections
+import nltk.metrics
+import nltk.classify.util
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import movie_reviews
+neg_ids = movie_reviews.fileids('neg')
+pos_ids = movie_reviews.fileids('pos')
+
+
+
+
+import itertools
+from nltk.collocations import BigramCollocationFinder
+from nltk.metrics import BigramAssocMeasures
+
+def bigram_word_feats(words, score_fn=BigramAssocMeasures.chi_sq, n=200):
+    bigram_finder = BigramCollocationFinder.from_words(words)
+    bigrams = bigram_finder.nbest(score_fn, n)
+    return dict([(ngram, True) for ngram in itertools.chain(words, bigrams)])
+neg_feats = [(bigram_word_feats(movie_reviews.words(fileids=[f])), 'neg') for f in neg_ids]
+pos_feats = [(bigram_word_feats(movie_reviews.words(fileids=[f])), 'pos') for f in pos_ids]
+
+neg_limit = len(neg_feats)*3/4
+pos_limit = len(pos_feats)*3/4
+
+
+trainfeats = neg_feats[:neg_limit] + pos_feats[:pos_limit]
+testfeats = neg_feats[neg_limit:] + pos_feats[pos_limit:]
+print 'train on %d instances, test on %d instances' % (len(trainfeats), len(testfeats))
+print neg_feats[1]
+
+classifier = NaiveBayesClassifier.train(trainfeats)
+import pickle
+f = open('bigram_classifier.pickle', 'wb')
+pickle.dump(classifier, f)
+f.close()
+print 'accuracy:', nltk.classify.util.accuracy(classifier, testfeats)
+classifier.show_most_informative_features()
+
+refsets = collections.defaultdict(set)
+testsets = collections.defaultdict(set)
+
+for i, (feats, label) in enumerate(testfeats):
+    refsets[label].add(i)
+    observed = classifier.classify(feats)
+    testsets[observed].add(i)
+
+print 'pos precision:', nltk.metrics.precision(refsets['pos'], testsets['pos'])
+print 'pos recall:', nltk.metrics.recall(refsets['pos'], testsets['pos'])
+print 'pos F-measure:', nltk.metrics.f_measure(refsets['pos'], testsets['pos'])
+print 'neg precision:', nltk.metrics.precision(refsets['neg'], testsets['neg'])
+print 'neg recall:', nltk.metrics.recall(refsets['neg'], testsets['neg'])
+print 'neg F-measure:', nltk.metrics.f_measure(refsets['neg'], testsets['neg'])
